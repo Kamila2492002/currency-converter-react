c0c82b12ae0361da54538542a7644e00a561ab32
Kamila Król <kamilakrol01@gmail.com>
2023-02-10 13:19:30 +0100

    added currencies.js

---
+++ src/currencies.js
@@ -0,0 +1,19 @@
+const currencies = [
+    {
+        name: "PLN",
+        toPLN: 1,
+
+    },
+    {   
+        name: "EUR",
+        toPLN: 4.76,
+
+    },
+    {   
+        name: "CHF",
+        toPLN: 5,
+
+    },
+]
+
+export default currencies; 
