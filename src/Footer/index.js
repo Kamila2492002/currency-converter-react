f7d37e792a08994a5198b8c534e5039ce6dadb69
Kamila Król <kamilakrol01@gmail.com>
2023-04-01 22:40:42 +0200

    delate footer

--- src/Footer/index.js
+++
@@ -1,9 +0,0 @@
-import { FooterHeading } from "./styled";
-
-const Footer = ({ title }) => (
-    <fooder>
-      <FooterHeading>{title}</FooterHeading>
-    </fooder>
-);
-
-export default Footer;
--- src/Footer/styled.js
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-export const FooterHeading = styled.h2`
-    width: 100%;
-    margin-left: 10 auto;
-    padding: 20px;
-    font-size: 12px;
-    text-align: center;
-`;
